a5dc490f207e637aea4937fa0bee41546dc5fc5b
PepperoniPingu <hugo.gris.se@gmail.com>
2022-12-02 17:22:54 +0100

    Read and write EEPROM
    Implemented functions to read and write to the EEPROM

--- eeprom.cpp
+++ eeprom.cpp
@@ -1,1 +1,21 @@
 #include "eeprom.h"
+
+void writeEEPROM(unsigned int memoryAddress, unsigned char data) {
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  Wire.write((unsigned char)(memoryAddress >> 8));   // MSB
+  Wire.write((unsigned char)(memoryAddress & 0xFF));
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+unsigned char readEEPROM(unsigned int memoryAddress) {
+  unsigned char rdata = 0xFF;
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  Wire.write((unsigned char)(memoryAddress >> 8));   // MSB
+  Wire.write((unsigned char)(memoryAddress & 0xFF));
+  Wire.endTransmission();
+  Wire.requestFrom(EEPROM_I2C_ADDRESS, 1);
+  while (!Wire.available()) {}
+  rdata = Wire.read();
+  return rdata;
+}
--- eeprom.h
+++ eeprom.h
@@ -1,5 +1,12 @@
 #ifndef eeprom_h
 #define eeprom_h
 
+#include <Wire.h>
+
+#define EEPROM_I2C_ADDRESS 0x50
+
+void writeEEPROM(unsigned int memoryAddress, unsigned char data);
+
+unsigned char readEEPROM(unsigned int memoryAddress);
 
 #endif
